e0089bb59ea4901eb5433301b7c26611b003b0d5
Yury Barysevich <yury_barysevich1@epam.com>
2021-03-29 17:27:17 +0300

    feat:add user photo

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,5 +1,8 @@
 const { OK, NO_CONTENT } = require('http-status-codes');
 const router = require('express').Router();
+const moment = require('moment');
+const fs = require('fs/promises');
+const cloudinary = require('cloudinary').v2;
 
 const userService = require('./user.service');
 const { id, user } = require('../../utils/validation/schemas');
@@ -9,7 +12,27 @@ const {
 } = require('../../utils/validation/validator');
 
 router.post('/', validator(user, 'body'), async (req, res) => {
-  const userEntity = await userService.save(req.body);
+  let userPhoto = '';
+  if (req.files) {
+    const file = req.files.imagine;
+    const date = moment().format('DDMMYYYY-HHmmss_SSS');
+    const fileFormat = file.name.split('.').pop();
+    file.name = `${date}.${fileFormat}`;
+    const pathimg = `./tmp/${file.name}`;
+    file.mv(pathimg, err => {
+      if (err) {
+        return res.status(200).json({ message: 'фото не загружено' });
+      }
+    });
+
+    const result = await cloudinary.uploader.upload(pathimg, {
+      upload_preset: 'rslang'
+    });
+    fs.unlink(pathimg);
+    userPhoto = result.secure_url;
+    console.log(result);
+  }
+  const userEntity = await userService.save({ ...req.body, userPhoto });
   res.status(OK).send(userEntity.toResponse());
 });
 
